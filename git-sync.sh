eaafdf639eae7241a3218b00ada43a9124231a3b
devnotepage <devnotepage@gmail.com>
2023-11-15 21:22:53 +0900

    Update

---
+++ git-sync.sh
@@ -0,0 +1,23 @@
+set LANG=ja_JP.UTF-8
+git config --global core.quotepath false
+
+#git add .
+#printf "\n"
+
+git status
+printf "\n"
+
+#read -p "Commit."
+
+#git commit -a -m'Update'
+#printf "\n"
+
+read -p "Sync."
+
+git pull -v --progress "origin"
+printf "\n"
+
+git push -v --progress "origin" main:main
+printf "\n"
+
+read -p "Complete."
