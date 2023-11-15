2da77e37e41f4893eb5fa902a1e39850db3b263e
devnotepage <devnotepage@gmail.com>
2023-11-15 21:57:07 +0900

    Update

--- cs-vb-comment-line-delete/cs-vb-comment-line-delete/FormMain.Designer.cs
+++ cs-vb-comment-line-delete/cs-vb-comment-line-delete/FormMain.Designer.cs
@@ -28,19 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.splitContainerMain = new System.Windows.Forms.SplitContainer();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerMain)).BeginInit();
+            this.splitContainerMain.Panel1.SuspendLayout();
+            this.splitContainerMain.Panel2.SuspendLayout();
+            this.splitContainerMain.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // splitContainerMain
+            // 
+            this.splitContainerMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerMain.Location = new System.Drawing.Point(0, 0);
+            this.splitContainerMain.Name = "splitContainerMain";
+            // 
+            // splitContainerMain.Panel1
+            // 
+            this.splitContainerMain.Panel1.Controls.Add(this.textBoxInput);
+            // 
+            // splitContainerMain.Panel2
+            // 
+            this.splitContainerMain.Panel2.Controls.Add(this.textBoxOutput);
+            this.splitContainerMain.Size = new System.Drawing.Size(480, 300);
+            this.splitContainerMain.SplitterDistance = 160;
+            this.splitContainerMain.TabIndex = 0;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxInput.Location = new System.Drawing.Point(0, 0);
+            this.textBoxInput.MaxLength = 999999999;
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(160, 300);
+            this.textBoxInput.TabIndex = 0;
+            this.textBoxInput.TextChanged += new System.EventHandler(this.textBoxInput_TextChanged);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxOutput.Location = new System.Drawing.Point(0, 0);
+            this.textBoxOutput.MaxLength = 999999999;
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(316, 300);
+            this.textBoxOutput.TabIndex = 0;
             // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(480, 300);
+            this.Controls.Add(this.splitContainerMain);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormMain";
             this.Text = "VBコメント行削除ツール";
+            this.splitContainerMain.Panel1.ResumeLayout(false);
+            this.splitContainerMain.Panel1.PerformLayout();
+            this.splitContainerMain.Panel2.ResumeLayout(false);
+            this.splitContainerMain.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerMain)).EndInit();
+            this.splitContainerMain.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainerMain;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxOutput;
     }
 }
--- cs-vb-comment-line-delete/cs-vb-comment-line-delete/FormMain.cs
+++ cs-vb-comment-line-delete/cs-vb-comment-line-delete/FormMain.cs
@@ -16,5 +16,31 @@ namespace cs_vb_comment_line_delete
         {
             InitializeComponent();
         }
+
+        private void textBoxInput_TextChanged(object sender, EventArgs e)
+        {
+            textBoxOutput.Text = DeleteCommentLine(textBoxInput.Text);
+        }
+
+        /// <summary>
+        /// VBコメント行を削除します。
+        /// </summary>
+        /// <param name="input">削除前の文字列</param>
+        /// <returns>削除後の文字列</returns>
+        private string DeleteCommentLine(string input)
+        {
+            try
+            {
+                var lines = input.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+                var deletedLines = lines.Where(x => !x.TrimStart().StartsWith("'"));
+                var deleted = string.Join(Environment.NewLine, deletedLines);
+                return deleted;
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.ToString());
+                return string.Empty;
+            }
+        }
     }
 }
